60cd4ada64209766b35c374c5caefc34fe4766d6
Dayasagar543 <dayasagar.543@gmail.com>
2025-09-23 20:12:12 +0530

    Add VISUAL_GRAPHICS_PYTHON

---
+++ v1_vpython_intro.py
@@ -0,0 +1,35 @@
+import vpython as vp #this is most commonly used in any dev environment .
+import time as tm
+import math as mt
+# from vpython  import *
+# import vpython
+# you can also import in 3 ways in python
+# ball=vp.sphere(color=vp.color.red)
+# tm.sleep(5)
+# mybox=vp.box(color=vp.color.orange,length=4,width=2,height=.5)
+# tm.sleep(6)
+# mycylinder=vp.cylinder(color=vp.color.magenta,radius=2,length=10,pos=vp.vector(-5,0,0))
+# tm.sleep(2)
+# mycylinder=vp.cylinder(color=vp.color.magenta,radius=2,width=10,height=6,pos=vp.vector(0,0,0))
+# tm.sleep(4)
+# mycylinder=vp.helix(color=vp.color.blue,radius=1,thickness=.2,coils=5,axis=vp.vector(0,5,0),pos=vp.vector(0,-2.5,0))
+# tm.sleep(4)
+floor=vp.box(color=vp.color.white,length=20,width=20,height=.5,pos=vp.vector(0,-10,0))
+ceiling=vp.box(color=vp.color.white,length=20,width=20,height=.5,pos=vp.vector(0,10,0))
+cylinder1=vp.cylinder(color=vp.color.green,radius=2,length=10,pos=vp.vector(0,-5,0),axis=vp.vector(0,1,0))
+while True:
+    # ball.color=vp.color.green
+    # tm.sleep(1)
+    # ball.color=vp.color.red
+    # tm.sleep(1) 
+    # ball.color=vp.color.blue
+    # tm.sleep(1)
+    # ball.color=vp.color.yellow
+    # tm.sleep(1)
+    # ball.color=vp.color.cyan        
+    # tm.sleep(1)
+    # ball.color=vp.color.magenta
+    # tm.sleep(1) 
+    pass
+# while True:
+#     pass
