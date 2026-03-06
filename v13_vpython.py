cb6821075d649e216473e60b1b3a33c7e31a74b2
Dayasagar543 <dayasagar.543@gmail.com>
2026-03-07 00:03:56 +0530

    added the code for the clock design and update the clock pinds need to arrange them in proper order

--- v13_vpython.py
+++ v13_vpython.py
@@ -16,6 +16,7 @@ arrow_thickness=0.01
 PointerX=arrow(length=arrow_length,shaftwidth=arrow_thickness,axis=vector(axis_x,y_home,z_home),color=color.red)
 PointerY=arrow(length=arrow_length,shaftwidth=arrow_thickness,axis=vector(x_home,axis_y,z_home),color=color.green)
 PointerZ=arrow(length=arrow_length,shaftwidth=arrow_thickness,axis=vector(x_home,y_home,axis_z),color=color.blue)
+ball=sphere(radius=arrow_length/100,color=color.cyan,make_trail=True,trail_color=color.orange)
 
 moving_arrow=arrow(length=arrow_length,shaftwidth=arrow_thickness,axis=vector(axis_x,axis_y,axis_z),color=color.yellow)
 
@@ -24,10 +25,18 @@ while True:
     for angle in np.linspace(0,2*np.pi,1000):
         rate(50)
         moving_arrow.axis=vector(arrow_length*np.sin(angle),arrow_length*np.cos(angle),z_home)
-    for angle in np.linspace(0,2*np.pi,1000):
-        rate(50)
-        moving_arrow.axis=vector(x_home,arrow_length*np.sin(angle),arrow_length*np.cos(angle))
+        ball.pos=vector(arrow_length*np.sin(angle),arrow_length*np.cos(angle),z_home)
+
     for angle in np.linspace(0,2*np.pi,1000):
         rate(50)
         moving_arrow.axis=vector(arrow_length*np.sin(angle),y_home,arrow_length*np.cos(angle))
+        ball.pos=vector(arrow_length*np.sin(angle),y_home,arrow_length*np.cos(angle))
+        
+    for angle in np.linspace(0,2*np.pi,1000):
+        rate(50)
+        moving_arrow.axis=vector(x_home,arrow_length*np.cos(angle),arrow_length*np.sin(angle))
+        ball.pos=vector(x_home,arrow_length*np.cos(angle),arrow_length*np.sin(angle))
+
+
+
     pass
--- v14_vpython.py
+++ v14_vpython.py
@@ -1,7 +1,30 @@
 from vpython import *
-import numpy as numpy
+import numpy as np
 
+null_value=0
+x_home=0
+y_home=0
+z_home=0
 
+x_active=1
+y_active=1
+z_active=1
+
+cylinder_length=0.2
+
+radius_clock=float(input("please enter the radius of the clock"))
+clock_disc=cylinder(radius=radius_clock,color=vector(x_home,125,110),pos=vector(x_home,y_home,z_home),axis=vector(x_home,y_home,-z_active),length=cylinder_length)
+major_tics_length=radius_clock/10
+major_tics_width=2*np.pi*radius_clock/100
+major_tics_height=radius_clock/20
+minor_tics_length=radius_clock/100
+minor_tics_width=major_tics_width/100
+minor_tics_height=radius_clock/100
 
 while True:
-    pass
+    for radians in np.linspace(null_value,2*np.pi,13):
+        major_Tics=box(color=color.red,pos=vector(radius_clock*np.cos(radians),radius_clock*np.sin(radians),z_home),axis=vector(radius_clock*np.cos(radians),radius_clock*np.sin(radians),z_active),length=major_tics_length,width=major_tics_width,height=major_tics_height)
+   
+    for radians in np.linspace(null_value,2*np.pi,67):
+        minor_Tics=box(color=color.blue,pos=vector(radius_clock*np.cos(radians),radius_clock*np.sin(radians),z_home),axis=vector(radius_clock*np.cos(radians),radius_clock*np.sin(radians),z_active),length=minor_tics_length,width=minor_tics_width,height=minor_tics_height)
+    pass                                   
